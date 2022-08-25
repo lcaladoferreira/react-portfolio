3f29bf07b8d5d9598263b059cda15e590c32b849
lcaladoferreira <83848600+lcaladoferreira@users.noreply.github.com>
2022-08-25 19:40:23 -0300

    sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,6 +4,8 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome, faUser, faEnvelope } from "@fortawesome/free-solid-svg-icons"
+import { faGithub, faLinkedin, faYoutube } from '@fortawesome/free-brands-svg-icons'
+
 
 const Sidebar = () => (
     <div className="nav-bar">
@@ -22,6 +24,32 @@ const Sidebar = () => (
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
             </NavLink>
         </nav>
+        <ul>
+            <li>
+                <a target="_blank" 
+                rel= "noreferrer" 
+                href="https://www.linkedin.com/in/lcaladoferreira/"
+                >
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a target="_blank" 
+                rel= "noreferrer" 
+                href="https://github.com/lcaladoferreira"
+                >
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a target="_blank" 
+                rel= "noreferrer" 
+                href="https://www.youtube.com/c/LCGuitarEDM"
+                >
+                    <FontAwesomeIcon icon={faYoutube} color="#4d4d4e" />
+                </a>
+            </li>
+        </ul>
 
     </div>
 
